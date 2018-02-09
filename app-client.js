6487c9a7587322feebecda327b8ee33b60299e8f
Manvendra Singh <manvendrask2@gmail.com>
2018-02-09 14:03:12 +0530

    Fixed import path.

--- app-client.js
+++ app-client.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import App from './components/app';
+import App from './components/App';
 
 import Audience from './components/Audience';
 import Board from './components/Board';
